d78e05769dc0d57d415665e618f9ddded9f4b1cc
ThanakornGuide <thanakornguide@gmail.com>
2022-06-21 18:14:08 +0700

    commit q3

---
+++ src/main/resources/db/migration/table_user_demo.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS user (
+    id  int NOT NULL AUTO_INCREMENT,
+    user_name VARCHAR(255),
+    yaer INT(4),
+    gender VARCHAR(50),
+    number_phone INT(10),
+    PRIMARY KEY (id)
+
+);
