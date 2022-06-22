b06f9fd9371b78f6169879965334fc6ade828f63
ThanakornGuide <thanakornguide@gmail.com>
2022-06-22 10:31:53 +0700

    commit q6

---
+++ src/main/resources/db/migration/V1__tbUser.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tbUser (
+    id integer PRIMARY KEY NOT NULL,
+    username varchar(255),
+    yaer integer,
+    gender varchar(50),
+    numberphone integer
+)
--- src/main/resources/db/migration/table_user_demo.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS tb_user (
-    id  int NOT NULL AUTO_INCREMENT,
-    user_name VARCHAR(255),
-    yaer INT(4),
-    gender VARCHAR(50),
-    number_phone INT(10),
-    PRIMARY KEY (id)
-
-);
