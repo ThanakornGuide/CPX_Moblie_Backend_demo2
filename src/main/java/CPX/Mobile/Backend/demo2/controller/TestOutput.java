5a0772732c829c8a6f23c77fa0f6703eaecf1d4b
ThanakornGuide <thanakornguide@gmail.com>
2022-06-22 11:55:38 +0700

    commit q6

--- src/main/java/CPX/Mobile/Backend/demo2/controller/TestOutput.java
+++ src/main/java/CPX/Mobile/Backend/demo2/controller/TestOutput.java
@@ -1,13 +1,19 @@
 package CPX.Mobile.Backend.demo2.controller;
 
+import CPX.Mobile.Backend.demo2.model.model_user;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class TestOutput {
+
     @GetMapping("/test_output")
     public String demo() {
-        return "Hello Backend Team";
+
+        model_user M_Uesr = new model_user();
+        M_Uesr.setName("test");
+        M_Uesr.setYear(123);
+
+        return Uesr;
     }
 }
---
+++ src/main/java/CPX/Mobile/Backend/demo2/model/model_user.java
@@ -0,0 +1,11 @@
+package CPX.Mobile.Backend.demo2.model;
+
+import lombok.Data;
+
+@Data
+public class model_user {
+    
+    private String name;
+    private Integer year;
+
+}
