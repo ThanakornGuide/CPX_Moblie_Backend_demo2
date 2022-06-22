079be9e72fa5a40b69f217d7049e4961b55d587b
ThanakornGuide <thanakornguide@gmail.com>
2022-06-22 15:46:53 +0700

    commit q6

--- src/main/java/CPX/Mobile/Backend/demo2/model/model_user.java
+++ src/main/java/CPX/Mobile/Backend/demo2/model/model_user.java
@@ -1,14 +1,12 @@
 package CPX.Mobile.Backend.demo2.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class model_user {
     
     private String user_name;
-    // private Integer year;
+    private Integer year;
     // private Double mom;
 
 }
