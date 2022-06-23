f38ee97152ab009a7a6dc16ee14d28f4abc44efd
ThanakornGuide <thanakornguide@gmail.com>
2022-06-23 12:00:50 +0700

    commit q7

--- src/main/java/CPX/Mobile/Backend/demo2/Demo2Application.java
+++ src/main/java/CPX/Mobile/Backend/demo2/Demo2Application.java
@@ -3,10 +3,6 @@ package CPX.Mobile.Backend.demo2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-import CPX.Mobile.Backend.demo2.model.userModel;
-
-
 @SpringBootApplication
 public class Demo2Application {
 
