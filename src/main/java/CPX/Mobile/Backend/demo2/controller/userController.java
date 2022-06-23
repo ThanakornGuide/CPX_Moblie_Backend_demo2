c50dbe2251e50d6987e1452a7488d16897afc2a9
ThanakornGuide <thanakornguide@gmail.com>
2022-06-23 17:05:40 +0700

    commit q7_12

--- src/main/java/CPX/Mobile/Backend/demo2/controller/userController.java
+++ src/main/java/CPX/Mobile/Backend/demo2/controller/userController.java
@@ -1,48 +1,74 @@
 package CPX.Mobile.Backend.demo2.controller;
 
 import java.net.URI;
+import java.util.List;
 
+import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import CPX.Mobile.Backend.demo2.model.userModel;
-import CPX.Mobile.Backend.demo2.model.usersModel;
-import CPX.Mobile.Backend.demo2.repository.userDAO;
+import CPX.Mobile.Backend.demo2.exception.ResourceNotFoundException;
+import CPX.Mobile.Backend.demo2.model.User;
+import CPX.Mobile.Backend.demo2.repository.userRepository;
 
 @RestController
-@RequestMapping(path = "/api")
+@RequestMapping(path = "/api/v1/user")
 public class userController {
 
+
     @Autowired
-    private userDAO this_userDAO;
+    private userRepository uRepository;
 
-    @GetMapping(path = "/v1/getAllUserList" , produces = "application/json")
-    public usersModel getUsers() {
-        return this_userDAO.getAllUsers();
+    @GetMapping
+    public List<User> getAllUser(){
+        return uRepository.findAll();
     }
 
-    @PostMapping(path= "/v1/postAddUser", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Object> addUser(@RequestBody userModel user) 
-    {
-        Integer id = this_userDAO.getAllUsers().getUserList().size() + 1;
-        user.setId(id);
-         
-        this_userDAO.addUser(user);
-         
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                                    .path("/{id}")
-                                    .buildAndExpand(user.getId())
-                                    .toUri();
-         
-        return ResponseEntity.created(location).build();
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return uRepository.save(user);
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserById(@PathVariable long id){
+        User user = uRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not exist with id:" + id));
 
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUserById(@PathVariable long id,@RequestBody User userDetails) {
+        User updateUser = uRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not exist with id:" + id));
+
+        updateUser.setUsername(userDetails.getUsername());
+        updateUser.setLastName(userDetails.getLastName());
+
+        uRepository.save(updateUser);
+
+        return ResponseEntity.ok(updateUser);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable long id){
+
+        User user = uRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not exist with id:" + id));
+
+        uRepository.delete(user);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
 
 }
