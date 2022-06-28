ea4b13e36df17871dfe9434e3ec5183fceeb40d6
ThanakornGuide <thanakornguide@gmail.com>
2022-06-28 09:25:26 +0700

    commit 18062022

--- src/main/java/CPX/Mobile/Backend/demo2/controller/userController.java
+++ src/main/java/CPX/Mobile/Backend/demo2/controller/userController.java
@@ -1,15 +1,19 @@
 package CPX.Mobile.Backend.demo2.controller;
 
+import java.io.IOException;
 import java.net.URI;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -18,9 +22,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import CPX.Mobile.Backend.demo2.exception.ErrorResponse;
 import CPX.Mobile.Backend.demo2.exception.UserNotFoundException;
 import CPX.Mobile.Backend.demo2.model.userModel;
 import CPX.Mobile.Backend.demo2.service.userService;
@@ -40,11 +46,46 @@ public class userController {
         return listUser;
     }
 
-    @PostMapping("/v1/user")
-    public ResponseEntity<userModel> createUser(@Valid @RequestBody userModel user) {
+    @PostMapping
+    @RequestMapping("/v1/user")
+    public ResponseEntity<?> createUser(@RequestBody userModel user){
+    
         
-        userModel saveUser = uService.createUser(user);
-        return new ResponseEntity<userModel>(saveUser, HttpStatus.CREATED);
+        // userModel saveUser = uService.createUser(user);
+        // return new ResponseEntity<userModel>(saveUser, HttpStatus.CREATED);
+
+        // if(user.getEmail().equals("null")){
+        //     ErrorResponse error = new ErrorResponse();
+        //     error.setStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+        //     error.setTimeStamp(LocalDateTime.now());
+        //     error.setMessage("Email is Empty"+user.getEmail().isEmpty());
+
+        
+        //     //return new ResponseEntity<ErrorResponse>(error, HttpStatus.OK);
+        //     throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Email is Empty "+user.getEmail().isEmpty());
+
+        // }else{
+        //     return null;
+        // }
+
+        try {
+            userModel saveUser = uService.createUser(user);
+            return new ResponseEntity<userModel>(saveUser, HttpStatus.CREATED);
+
+        }catch (IOException e) {
+           ErrorResponse error = new ErrorResponse();
+            error.setStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+            error.setTimeStamp(LocalDateTime.now());
+            error.setMessage("Email is Empty");
+
+        
+            return new ResponseEntity<ErrorResponse>(error, HttpStatus.UNPROCESSABLE_ENTITY);
+            //return IOException()
+            //throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Email is Empty "+user.getEmail().isEmpty());
+
+        }
+            
+    
     }
 
     @GetMapping("/v1/user/{id}")
